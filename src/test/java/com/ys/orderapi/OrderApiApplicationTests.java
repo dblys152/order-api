e8f9a5dfbd914c24c48f04bfbacc3db30fc467c6
youngseok <dblys152@gmail.com>
2023-05-31 19:38:36 +0900

    프로젝트 생성

---
+++ src/test/java/com/ys/orderapi/OrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.orderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
