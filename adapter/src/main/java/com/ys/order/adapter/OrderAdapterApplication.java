9d7357728d38dec4c23e7b6b91754081eba5aeb5
youngseok <dblys152@gmail.com>
2023-06-01 15:16:38 +0900

    모듈 설정

---
+++ adapter/src/main/java/com/ys/order/adapter/OrderAdapterApplication.java
@@ -0,0 +1,15 @@
+package com.ys.order.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.ys.stock")
+public class OrderAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderAdapterApplication.class, args);
+	}
+
+}
